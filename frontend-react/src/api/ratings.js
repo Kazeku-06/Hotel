02049a3f6b947b25b748973869cfc0957032a3b2
nopalll <tssytari@gmail.com>
2025-11-26 21:01:25 +0700

    kurang fitur ratings dan fixing bug kamar status ketika masih di booking

--- frontend-react/src/api/ratings.js
+++ frontend-react/src/api/ratings.js
@@ -1,6 +1,15 @@
 import api from './axios'
 
 export const ratingsAPI = {
+  // For users to submit ratings
   createRating: (data) => api.post('/ratings', data),
+  
+  // For users to get their ratings
   getRatings: () => api.get('/ratings'),
+  
+  // For admin to get all ratings
+  getAdminRatings: () => api.get('/admin/ratings'),
+  
+  // Get ratings for specific booking
+  getBookingRatings: (bookingId) => api.get(`/ratings/booking/${bookingId}`),
 }
